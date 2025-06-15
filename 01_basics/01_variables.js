f223cdff05a162f773722252cae3e1f9afb85e30
Keshav Kunal <infini2ude@gmail.com>
2025-06-15 15:02:49 +0000

    study on datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 7585
+let accountEmail = " kk@king.com"
+var accountPass ="12345" // we do not prefer to use var as issue in block scope and functional scope
+accountCity = "Blr"  // not preferred 
+let state
+// accountId = 2 --> Not allowed
+accountEmail= "kunal@king.com"
+accountPass = "23332"
+accountCity = "Delhi"
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountEmail, accountId, accountPass, accountCity, state])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as  newer version
+// alert( 3+3 ) --> as we are using node js, not browser
+
+console.log(3
+    + 3) // it runs but wrong practice
+
+let name = "Keshav"
+let age = 24
+let isLoggedIN = false
+
+// number -> 2 ^53
+// bigint
+//string -> ""
+//  boolean -> true/false
+// null -> standalone value
+// undefined
+//  symbol -> to find uniqueness 
+
+// object 
+
+console.log(typeof 3)
+console.log(typeof "KK");
+
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
+
+
